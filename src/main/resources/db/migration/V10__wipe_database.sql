034742944c5a535b5d1261d9d62221867e515eab
Kevin Sillerud <kevin.sillerud@gmail.com>
2020-11-04 09:34:33 +0100

    Fix typo in migration

--- src/main/resources/db/migration/V10__wipe_database.sql
+++ src/main/resources/db/migration/V10__wipe_database.sql
@@ -1,7 +1,7 @@
 DELETE FROM warning;
 DELETE FROM begrunnelse;
 DELETE FROM godkjenning;
-DELETE FROM godkjenningsbehov_warnings;
+DELETE FROM godkjenningsbehov_warning;
 DELETE FROM godkjenningsbehov_losning_begrunnelse;
 DELETE FROM godkjenningsbehov_losning;
 DELETE FROM godkjenningsbehov;
