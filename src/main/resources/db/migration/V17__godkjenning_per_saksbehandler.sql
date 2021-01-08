91b0b844cf8c78bb963323c34d22cae146b5a33c
Kevin Sillerud <kevin.sillerud@gmail.com>
2021-01-08 10:00:51 +0100

    Gjør klar for en replay av meldinger
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>

--- src/main/resources/db/migration/V17__godkjenning_per_saksbehandler.sql
+++ src/main/resources/db/migration/V17__godkjenning_per_saksbehandler.sql
@@ -1,6 +1,18 @@
 DELETE FROM godkjenningsbehov_losning;
-ALTER TABLE godkjenningsbehov_losning ADD COLUMN godkjent_av VARCHAR(36) NOT NULL;
+ALTER TABLE godkjenningsbehov_losning ADD COLUMN godkjent_av VARCHAR(36);
 
 ALTER TABLE vedtaksperiode_aktivitet ADD COLUMN hendelse_id UUID;
 ALTER TABLE vedtaksperiode_tilstandsendring ADD COLUMN hendelse_id UUID;
-ALTER TABLE godkjenningsbehov ALTER COLUMN periodetype DROP NOT NULL;
+
+ALTER TABLE godkjenningsbehov ALTER COLUMN periodetype SET NOT NULL;
+ALTER TABLE godkjenningsbehov_losning ALTER COLUMN godkjent_av SET NOT NULL;
+
+DELETE FROM warning;
+DELETE FROM begrunnelse;
+DELETE FROM godkjenning;
+DELETE FROM godkjenningsbehov_warning;
+DELETE FROM godkjenningsbehov_losning_begrunnelse;
+DELETE FROM godkjenningsbehov_losning;
+DELETE FROM godkjenningsbehov;
+DELETE FROM vedtaksperiode_tilstandsendring;
+DELETE FROM vedtaksperiode_aktivitet;
