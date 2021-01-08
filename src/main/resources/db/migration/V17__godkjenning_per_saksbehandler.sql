8fc1fef673607d5442f00fc7f170354483289afe
Kevin Sillerud <kevin.sillerud@gmail.com>
2021-01-08 10:20:07 +0100

    Gjør felter non-nullable etter tabellene er tomme
    Co-authored-by: Simen Ullern <simen.ullern@nav.no>

--- src/main/resources/db/migration/V17__godkjenning_per_saksbehandler.sql
+++ src/main/resources/db/migration/V17__godkjenning_per_saksbehandler.sql
@@ -4,9 +4,6 @@ ALTER TABLE godkjenningsbehov_losning ADD COLUMN godkjent_av VARCHAR(36);
 ALTER TABLE vedtaksperiode_aktivitet ADD COLUMN hendelse_id UUID;
 ALTER TABLE vedtaksperiode_tilstandsendring ADD COLUMN hendelse_id UUID;
 
-ALTER TABLE godkjenningsbehov ALTER COLUMN periodetype SET NOT NULL;
-ALTER TABLE godkjenningsbehov_losning ALTER COLUMN godkjent_av SET NOT NULL;
-
 DELETE FROM warning;
 DELETE FROM begrunnelse;
 DELETE FROM godkjenning;
@@ -16,3 +13,6 @@ DELETE FROM godkjenningsbehov_losning;
 DELETE FROM godkjenningsbehov;
 DELETE FROM vedtaksperiode_tilstandsendring;
 DELETE FROM vedtaksperiode_aktivitet;
+
+ALTER TABLE godkjenningsbehov ALTER COLUMN periodetype SET NOT NULL;
+ALTER TABLE godkjenningsbehov_losning ALTER COLUMN godkjent_av SET NOT NULL;
