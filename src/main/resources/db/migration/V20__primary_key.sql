f36d2a5d3f0c63db8d8749c4db1975c67139e6ac
Kevin Sillerud <kevin.sillerud@gmail.com>
2021-03-15 11:23:38 +0100

    Sletter duplikater før vi legger til PK
    Co-authored-by: Anders Østby <anders.ostby@nav.no>
    Co-authored-by: Oscar Vladau-Husevold <oscar.vladau-husevold@nav.no>

--- src/main/resources/db/migration/V20__primary_key.sql
+++ src/main/resources/db/migration/V20__primary_key.sql
@@ -1,3 +1,7 @@
+DELETE FROM godkjenningsbehov_losning a USING godkjenningsbehov_losning b WHERE a.CTID < b.CTID AND a.id = b.id;
+DELETE FROM godkjenningsbehov_losning_begrunnelse a USING godkjenningsbehov_losning_begrunnelse b WHERE a.CTID < b.CTID AND a.id = b.id;
+DELETE FROM vedtaksperiode_tilstandsendring a USING vedtaksperiode_tilstandsendring b WHERE a.CTID < b.CTID AND a.hendelse_id = b.hendelse_id;
+
 ALTER TABLE godkjenningsbehov_losning ADD PRIMARY KEY (id);
 ALTER TABLE godkjenningsbehov_losning_begrunnelse ADD PRIMARY KEY (id);
 ALTER TABLE vedtaksperiode_tilstandsendring RENAME COLUMN hendelse_id TO id;
