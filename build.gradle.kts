aeecf5aabd928800555acf7b7754b59dd170f61b
Jonas Hasle <jonas.hasle@nav.no>
2023-01-23 16:03:17 +0100

    Ruller tilbake versjonsbumper
    - Tester og ser om det fungerer med gamle versjoner, ble mer omfattende å bumpe alt da det var noen major versions med større endringer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,19 @@
 val junitJupiterVersion = "5.7.1"
 val testcontainersVersion = "1.16.2"
-val rapidsAndRiversVersion = "2022122313141671797650.f806f770805a"
 
 plugins {
-    kotlin("jvm") version "1.7.21"
+    kotlin("jvm") version "1.6.10"
 }
 
 group = "no.helse"
 
-val githubUser: String by project
-val githubPassword: String by project
-
 repositories {
     mavenCentral()
-    maven {
-        url = uri("https://maven.pkg.github.com/navikt/*")
-        credentials {
-            username = githubUser
-            password = githubPassword
-        }
-    }
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:$rapidsAndRiversVersion")
+    implementation("com.github.navikt:rapids-and-rivers:2022.04.05-09.40.11a466d7ac70")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("no.nav:vault-jdbc:1.3.10")
     implementation("org.flywaydb:flyway-core:8.4.1")
@@ -77,6 +67,6 @@ tasks {
     }
 
     withType<Wrapper> {
-        gradleVersion = "7.4.2"
+        gradleVersion = "7.0"
     }
 }
