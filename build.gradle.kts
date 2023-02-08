c1a351f714760458c9863e8f72823f36deeeb205
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-02-08 22:25:34 +0100

    Forsøk å bumpe kotlin og r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ val postgresqlVersion = "42.5.1"
 val ktorVersion = "2.2.3"
 
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.8.0"
 }
 
 group = "no.helse"
@@ -16,7 +16,7 @@ repositories {
 
 dependencies {
     implementation("org.postgresql:postgresql:$postgresqlVersion")
-    implementation("com.github.navikt:rapids-and-rivers:2022.04.05-09.40.11a466d7ac70")
+    implementation("com.github.navikt:rapids-and-rivers:2022122313141671797650.f806f770805a")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("org.flywaydb:flyway-core:8.4.1")
     implementation("com.github.seratch:kotliquery:1.3.1")
