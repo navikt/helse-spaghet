a673ba82895101c246d52aa7a20e7cb5581ee113
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2023-11-02 20:01:52 +0100

    Lager en main som gjør det enklere å se hvordan meldingen blir 💡

--- src/main/kotlin/no/nav/helse/ventetilstand/OppsummerVedtaksperiodeVenter.kt
+++ src/main/kotlin/no/nav/helse/ventetilstand/OppsummerVedtaksperiodeVenter.kt
@@ -5,6 +5,7 @@ import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
 import no.nav.helse.ventetilstand.Slack.sendPåSlack
+import no.nav.helse.ventetilstand.VedtaksperiodeVentetilstandDao.Ventegruppe
 import org.slf4j.LoggerFactory
 import org.slf4j.event.Level.INFO
 import javax.sql.DataSource
@@ -37,22 +38,7 @@ internal class OppsummerVedtaksperiodeVenter (
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         try {
             val (oppsummering, antallPersoner) = dao.oppsummering()
-
-            val antallVedtaksperioder = oppsummering.sumOf { it.antall }
-            val propper = oppsummering.filter { it.propp }
-            val ettergølgende = oppsummering.filterNot { it.propp }.sumOf { it.antall }
-
-            var melding =
-                "\n\nDet er $antallVedtaksperioder vedtaksperioder fordelt på $antallPersoner sykmeldte som venter i systemet ⏳\n\n"
-
-            val antallPropper = propper.sumOf { it.antall }
-            melding += "Av disse er det $antallPropper som venter på noe direkte:\n"
-            propper.forEach { (årsak, antall) ->
-                melding += "${antall.fintAntall} venter på ${årsak.finÅrsak} ${antall.finProsentAv(antallPropper)}\n"
-            }
-
-            melding += "\n\nDe resterende $ettergølgende står bak en av ☝️ og venter tålmodig :sonic-waiting:\n"
-
+            val melding = lagMelding(oppsummering, antallPersoner)
             context.sendPåSlack(packet, INFO, melding)
         } catch (exception: Exception) {
             sikkerlogg.error("Feil ved generering av oppsummering for vedtaksperioder som venter", exception)
@@ -75,6 +61,52 @@ internal class OppsummerVedtaksperiodeVenter (
             val prosent = String.format("%.2f", it)
             if ("0.00" == prosent || "0,00" == prosent) ""
             else "($prosent%)"
+        }
+
+        private fun lagMelding(oppsummering: List<Ventegruppe>, antallPersoner: Int): String {
+            val antallVedtaksperioder = oppsummering.sumOf { it.antall }
+            val propper = oppsummering.filter { it.propp }
+            val ettergølgende = oppsummering.filterNot { it.propp }.sumOf { it.antall }
+
+            var melding =
+                "\n\nDet er $antallVedtaksperioder vedtaksperioder fordelt på $antallPersoner sykmeldte som venter i systemet ⏳\n\n"
+
+            val antallPropper = propper.sumOf { it.antall }
+            melding += "Av disse er det $antallPropper som venter på noe direkte:\n"
+            propper.forEach { (årsak, antall) ->
+                melding += "${antall.fintAntall} venter på ${årsak.finÅrsak} ${antall.finProsentAv(antallPropper)}\n"
+            }
+
+            melding += "\n\nDe resterende $ettergølgende står bak en av ☝️ og venter tålmodig :sonic-waiting:\n"
+
+            return melding
+        }
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val enOppsummering = """
+                INNTEKTSMELDING,true,33376
+                GODKJENNING,false,14300
+                GODKJENNING_FORDI_OVERSTYRING_IGANGSATT,false,11327
+                GODKJENNING,true,10575
+                GODKJENNING_FORDI_OVERSTYRING_IGANGSATT,true,4194
+                INNTEKTSMELDING,false,3424
+                INNTEKTSMELDING_FORDI_MANGLER_INNTEKT_FOR_VILKÅRSPRØVING_PÅ_ANDRE_ARBEIDSGIVERE,true,338
+                INNTEKTSMELDING_FORDI_MANGLER_INNTEKT_FOR_VILKÅRSPRØVING_PÅ_ANDRE_ARBEIDSGIVERE,false,319
+                SØKNAD_FORDI_HAR_SYKMELDING_SOM_OVERLAPPER_PÅ_ANDRE_ARBEIDSGIVERE,true,171
+                INNTEKTSMELDING_FORDI_MANGLER_REFUSJONSOPPLYSNINGER_PÅ_ANDRE_ARBEIDSGIVERE,false,94
+                SØKNAD_FORDI_HAR_SYKMELDING_SOM_OVERLAPPER_PÅ_ANDRE_ARBEIDSGIVERE,false,87
+                INNTEKTSMELDING_FORDI_MANGLER_REFUSJONSOPPLYSNINGER_PÅ_ANDRE_ARBEIDSGIVERE,true,59
+                INNTEKTSMELDING_FORDI_MANGLER_TILSTREKKELIG_INFORMASJON_TIL_UTBETALING_SAMME_ARBEIDSGIVE,false,37
+                INNTEKTSMELDING_FORDI_MANGLER_TILSTREKKELIG_INFORMASJON_TIL_UTBETALING_SAMME_ARBEIDSGIVE,true,15
+                HJELP_FORDI_VIL_UTBETALES,true,2
+                BEREGNING_FORDI_OVERSTYRING_IGANGSATT,true,1
+                BEREGNING_FORDI_OVERSTYRING_IGANGSATT,false,1
+            """.trimIndent().lines().map {
+                val split = it.split(",")
+                Ventegruppe(split[0], split[2].toInt(), split[1].toBoolean())
+            }
+            println(lagMelding(oppsummering = enOppsummering, antallPersoner = 33521))
         }
     }
 }
