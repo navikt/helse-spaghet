250031a6e010a6fa70c06a984321732471582103
david steinsland <david@davidsteinsland.net>
2023-12-08 08:33:20 +0100

    utveksler spannerlink i prod

--- src/main/kotlin/no/nav/helse/ventetilstand/IdentifiserStuckVedtaksperioder.kt
+++ src/main/kotlin/no/nav/helse/ventetilstand/IdentifiserStuckVedtaksperioder.kt
@@ -1,10 +1,18 @@
 package no.nav.helse.ventetilstand
 
+import com.fasterxml.jackson.core.JsonParseException
+import com.fasterxml.jackson.databind.ObjectMapper
+import no.nav.helse.objectMapper
 import no.nav.helse.rapids_rivers.*
 import no.nav.helse.ventetilstand.Slack.sendPåSlack
 import org.slf4j.LoggerFactory
 import org.slf4j.event.Level.ERROR
 import org.slf4j.event.Level.INFO
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.time.Duration
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 import java.util.*
@@ -51,19 +59,18 @@ internal class IdentifiserStuckVedtaksperioder (
             val venterPå = stuck
                 .groupBy { it.fødselsnummer }
                 .mapValues { (_, vedtaksperioder) -> vedtaksperioder.first().venterPå }
-                .values
-                .filterNot {  it.skalIkkeMase }
+                .filterNot {  it.value.skalIkkeMase }
                 .takeUnless { it.isEmpty() } ?: return ingentingStuck(packet, context)
 
             var melding =
                 "\n\nDet er ${stuck.size.vedtaksperioder} som ser ut til å være stuck! :helene-redteam:\n" +
                 "Fordelt på ${venterPå.size.personer}:\n\n"
 
-            venterPå.forEachIndexed { index, it ->
-                melding += "\t${index+1}) ${it.kibanaUrl} venter på ${it.snygg}"
-                if (venterPå.lastIndex != index) melding += "\n"
+            var index = 0
+            melding += venterPå.entries.joinToString(separator = "\n") { (fnr, vedtaksperiode) ->
+                index += 1
+                "\t$index) ${vedtaksperiode.kibanaUrl} venter på ${vedtaksperiode.snygg} ${fnr.spannerUrl?.let { "($it)" }}"
             }
-
             context.sendPåSlack(packet, ERROR, melding)
         } catch (exception: Exception) {
             sikkerlogg.error("Feil ved identifisering av stuck vedtaksperioder", exception)
@@ -71,10 +78,14 @@ internal class IdentifiserStuckVedtaksperioder (
     }
 
     private companion object {
+        private val spurteDuClient = SpurteDuClient()
         private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
         private val VenterPå.snygg get() = if (hvorfor == null) hva else "$hva fordi $hvorfor"
         private val VenterPå.kibanaUrl get() = "https://logs.adeo.no/app/kibana#/discover?_a=(index:'tjenestekall-*',query:(language:lucene,query:'%22${vedtaksperiodeId}%22'))&_g=(time:(from:'${LocalDateTime.now().minusDays(1)}',mode:absolute,to:now))".let { url ->
             "<$url|$vedtaksperiodeId>"
+        }
+        private val String.spannerUrl get() = spurteDuClient.utveksleUrl("https://spanner.intern.nav.no/person/${this}")?.let { url ->
+            "<$url|spannerlink>"
         }
 
         /** Liste med perioder som er stuck, men bruker kontaktes av saksbehandler for å avvente ny informasjon som kan endre på stuck-situasjonen **/
@@ -92,5 +103,44 @@ internal class IdentifiserStuckVedtaksperioder (
 
         private val Int.personer get() = if (this == 1) "én person" else "$this personer"
         private val Int.vedtaksperioder get() = if (this == 1) "én vedtaksperiode" else "$this vedtaksperioder"
+    }
+}
+
+class SpurteDuClient(private val host: String) {
+    constructor() : this(when (System.getenv("NAIS_CLUSTER_NAME")) {
+        "prod-gcp" -> "https://spurte-du.intern.nav.no"
+        else -> "https://spurte-du.intern.dev.nav.no"
+    })
+    private companion object {
+        private const val tbdgruppeProd = "f787f900-6697-440d-a086-d5bb56e26a9c"
+    }
+
+    fun utveksleUrl(url: String) = utveksleSpurteDu(objectMapper, mapOf(
+        "url" to url,
+        "påkrevdTilgang" to tbdgruppeProd
+    ))?.let { path ->
+        host + path
+    }
+    private fun utveksleSpurteDu(objectMapper: ObjectMapper, data: Map<String, String>): String? {
+        val httpClient = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(10))
+            .build()
+
+        val jsonInputString = objectMapper.writeValueAsString(data)
+
+        val request = HttpRequest.newBuilder()
+            .uri(URI("http://spurtedu/skjul_meg"))
+            .timeout(Duration.ofSeconds(10))
+            .header("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(jsonInputString))
+            .build()
+
+        val response = httpClient.send(request, HttpResponse.BodyHandlers.ofString())
+
+        return try {
+            objectMapper.readTree(response.body()).path("path").asText()
+        } catch (err: JsonParseException) {
+            null
+        }
     }
 }
