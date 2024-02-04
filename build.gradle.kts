bd3653159af2c0d8ea802d81679a4b6b321ec249
david steinsland <david@davidsteinsland.net>
2024-02-04 22:17:10 +0100

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 
 dependencies {
     implementation("org.postgresql:postgresql:$postgresqlVersion")
-    implementation("com.github.navikt:rapids-and-rivers:2024020421571707080262.b74e641fd6dd")
+    implementation("com.github.navikt:rapids-and-rivers:2024020422121707081126.d834a5d05bb2")
     implementation("com.zaxxer:HikariCP:5.0.1")
     implementation("org.flywaydb:flyway-core:8.4.1")
     implementation("com.github.seratch:kotliquery:1.3.1")
