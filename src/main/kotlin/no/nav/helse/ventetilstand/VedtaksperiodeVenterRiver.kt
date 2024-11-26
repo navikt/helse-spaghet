cfbc4496a2d29ff17e9b079cd35c3a67199379da
david steinsland <david@davidsteinsland.net>
2024-11-26 12:12:25 +0100

    fjerner vedtaksperiode_venter-river til fordel for ny

--- src/main/kotlin/no/nav/helse/ventetilstand/VedtaksperiodeVenterRiver.kt
+++ src/main/kotlin/no/nav/helse/ventetilstand/VedtaksperiodeVenterRiver.kt
@@ -34,31 +34,6 @@ internal class VedtaksperiodeVenterRiver (
                 }
             }
         }.register(this)
-
-        // todo: deprecated river
-        River(rapidApplication).apply {
-            precondition { it.requireValue("@event_name", "vedtaksperiode_venter") }
-            validate { it.requireKey(
-                "venterPå.venteårsak.hva",
-                "venterPå.vedtaksperiodeId",
-                "venterPå.skjæringstidspunkt",
-                "venterPå.organisasjonsnummer"
-            ) }
-            validate { it.interestedIn("venterPå.venteårsak.hvorfor") }
-            validate { it.requireKey(
-                "@id",
-                "vedtaksperiodeId",
-                "skjæringstidspunkt",
-                "organisasjonsnummer",
-                "ventetSiden",
-                "venterTil",
-                "fødselsnummer"
-            ) }
-        }.register(object : River.PacketListener {
-            override fun onPacket(packet: JsonMessage, context: MessageContext, metadata: MessageMetadata, meterRegistry: MeterRegistry) {
-                dao.venter(packet.vedtaksperiodeVenter, packet.hendelse)
-            }
-        })
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext, metadata: MessageMetadata, meterRegistry: MeterRegistry) {
@@ -80,24 +55,6 @@ internal class VedtaksperiodeVenterRiver (
                 )
             ), packet.hendelse)
         }
-    }
-
-    private companion object {
-        val JsonMessage.vedtaksperiodeVenter get() = VedtaksperiodeVenter.opprett(
-            vedtaksperiodeId = UUID.fromString(this["vedtaksperiodeId"].asText()),
-            skjæringstidspunkt = this["skjæringstidspunkt"].asLocalDate(),
-            fødselsnummer = this["fødselsnummer"].asText(),
-            organisasjonsnummer = this["organisasjonsnummer"].asText(),
-            ventetSiden = this["ventetSiden"].asLocalDateTime(),
-            venterTil = this["venterTil"].asLocalDateTime(),
-            venterPå = VenterPå(
-                vedtaksperiodeId = UUID.fromString(this["venterPå.vedtaksperiodeId"].asText()),
-                skjæringstidspunkt = this["venterPå.skjæringstidspunkt"].asLocalDate(),
-                organisasjonsnummer = this["venterPå.organisasjonsnummer"].asText(),
-                hva = this["venterPå.venteårsak.hva"].asText(),
-                hvorfor = this["venterPå.venteårsak.hvorfor"].takeUnless { it.isMissingOrNull() }?.asText()
-            )
-        )
     }
 }
 
