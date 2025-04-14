fb7c0a648663e715498340338836f2101f272ddb
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2025-04-14 12:53:13 +0200

    Bare viser at typen på melding fra Spedisjon kan være hva som helst

--- src/test/kotlin/no/nav/helse/E2eTestApp.kt
+++ src/test/kotlin/no/nav/helse/E2eTestApp.kt
@@ -23,7 +23,7 @@ import org.slf4j.LoggerFactory
 private val tables = "annullering,annullering_arsak,begrunnelse,flyway_schema_history,funksjonell_feil,godkjenning,godkjenning_overstyringer,godkjenningsbehov,godkjenningsbehov_losning,godkjenningsbehov_losning_begrunnelse,hendelse_ikke_håndtert_årsak,oppgave,oppgave_endret,regelverksvarsel,revurdering,revurdering_vedtaksperiode,schedule,skatteinntekter_lagt_til_grunn,soknad,soknad_haandtert,utkast_til_vedtak,varsel,vedtaksperiode_aktivitet,vedtaksperiode_data,vedtaksperiode_tilstandsendring,vedtaksperiode_venter,warning_for_godkjenning,lagt_paa_vent"
 private val databaseContainer = DatabaseContainers.container("spaghet", CleanupStrategy.tables(tables), walLevelLogical = true)
 
-class E2eTestApp() {
+class E2eTestApp {
     var rapid: TestRapid = TestRapid()
         private set
     private var listAppender: ListAppender<ILoggingEvent> = ListAppender()
@@ -46,7 +46,7 @@ class E2eTestApp() {
     val spedisjonClient = mockk<SpedisjonClient> {
         every { hentMelding(any(), any()) } answers {
             HentMeldingResponse(
-                type = "inntektsmelding",
+                type = "denne_type_kan_være_hva_som_helst_fordi_spaghet_bare_tar_ekstern_dokument_id_uansett",
                 fnr = "99999999999",
                 internDokumentId = UUID.randomUUID(),
                 eksternDokumentId = UUID.randomUUID(),
