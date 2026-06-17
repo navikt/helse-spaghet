e7ed2dd5f7fbbc0d82948ef47a258d1e7f639bab
Eirik Gallefoss <eirik@kakepepper.org>
2026-06-17 10:01:18 +0200

    🗃️ Fjern rettigheter uten å gjøre reassign

--- src/main/resources/db/migration/V86__fjern_rettigheter_spaghetti.sql
+++ src/main/resources/db/migration/V86__fjern_rettigheter_spaghetti.sql
@@ -1,13 +1,11 @@
 DO $$BEGIN
     IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'spaghetti') THEN
-        REASSIGN OWNED BY "spaghetti" TO "spaghet2";
-
         REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM "spaghetti";
         REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM "spaghetti";
 
-        ALTER DEFAULT PRIVILEGES FOR USER spaghet IN SCHEMA public
+        ALTER DEFAULT PRIVILEGES FOR USER spaghet2 IN SCHEMA public
             REVOKE ALL PRIVILEGES ON TABLES FROM "spaghetti";
-        ALTER DEFAULT PRIVILEGES FOR USER spaghet IN SCHEMA public
+        ALTER DEFAULT PRIVILEGES FOR USER spaghet2 IN SCHEMA public
             REVOKE ALL PRIVILEGES ON SEQUENCES FROM "spaghetti";
     END IF;
 END $$;
